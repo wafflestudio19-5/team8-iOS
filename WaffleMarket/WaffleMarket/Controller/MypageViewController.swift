031c53cb8a613fe0d981417a96715828ff3c1071
VioletXF <zudevxf@gmail.com>
2022-01-05 23:41:29 +0900

    Feat: Add mypage view controller

---
+++ WaffleMarket/WaffleMarket/Controller/MypageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MypageViewController.swift
+//  WaffleMarket
+//
+//  Created by 안재우 on 2022/01/05.
+//
+
+import Foundation
+import UIKit
+class MypageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
