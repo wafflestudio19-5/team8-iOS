9a1f60405da7c9577302abf2981acba5038e60d5
shinehardd <chaemin2001@gmail.com>
2022-01-26 01:07:06 +0900

    feat-article-list

---
+++ WaffleMarket/WaffleMarket/Controller/ArticleListViewController.swift
@@ -0,0 +1,203 @@
+//
+//  ArticleListViewController.swift
+//  WaffleMarket
+//
+//  Created by Chaemin Lee on 2022/01/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+private let reuseIdentifier = "Cell"
+
+// TODO: 백엔드 연결
+//class ArticleListViewModel: ObservableObject {
+//
+//    var page = 1
+//    var articles = [Article]()
+//    let articleList = BehaviorRelay<[Article]>(value: [])
+//    let disposeBag = DisposeBag()
+//    var isLoadingMoreData = false
+//
+//    func getArticleList(page:Int, listName: String? = nil, append: Bool = false) {
+//
+//        let prevPage = page - 1
+//        // 백엔드와 연결시 API 호출
+//        isLoadingMoreData = true
+//
+//        ArticleAPI.privateList(page: page, listName: listName).subscribe { response in
+//            print(String(decoding: response.data, as: UTF8.self))
+//            let decoder = JSONDecoder()
+//            if let decoded = try? decoder.decode([ArticleResponse].self, from: response.data){
+//                var articles: [Article] = []
+//
+//                for articleResponse in decoded {
+//                    var thumbnail: String? = nil
+//                    if articleResponse.product_images.count > 0 {
+//                        thumbnail = articleResponse.product_images[0].thumbnail_url
+//                    }
+//                    let article = Article(
+//                        id: articleResponse.id,
+//                        title: articleResponse.title,
+//                        category: articleResponse.category,
+//                        price: articleResponse.price,
+//                        content: articleResponse.content,
+//                        productImages: articleResponse.product_images.map({ it in
+//                            it.image_url
+//                        }),
+//                        thumbnailImage: thumbnail,
+//                        isSold: (articleResponse.buyer != nil)
+//
+//                    )
+//                    articles.append(article)
+//                }
+//                print("articles count:", articles.count)
+//                if append {
+//                    self.articleList.accept(self.articleList.value + articles)
+//                } else {
+//                    self.articleList.accept(articles)
+//                }
+//            } else if let decoded = try? decoder.decode(NonFieldErrorsResponse.self, from: response.data){
+//                if decoded.non_field_errors.count>0 && decoded.non_field_errors[0] == "페이지 번호가 범위를 벗어났습니다." {
+//                    self.page = prevPage
+//                    if !append{
+//                        self.articleList.accept([])
+//                    }
+//                }
+//
+//            } else {
+//                print("decoding failure")
+//            }
+//            self.isLoadingMoreData = false
+//        } onFailure: { error in
+//            self.isLoadingMoreData = false
+//            print(error)
+//        } onDisposed: {
+//
+//        }.disposed(by: disposeBag)
+//    }
+//
+//    func getSelectedList(listName: String, page: Int) {
+//        // categorypicker로 카테고리 선택시 호출
+//    }
+//
+//    func getArticleAt(_ index: IndexPath) -> Article?{
+//
+//        let article = articleList.value[index.item]
+//        return article
+//    }
+//}
+
+
+class ArticleListViewController: UIViewController, UITableViewDelegate {
+
+    let articleTableView = UITableView()
+    let imageLoader = CachedImageLoader()
+    let disposeBag = DisposeBag()
+    let viewModel = ArticleViewModel()
+    var listName: String?
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.view.addSubview(articleTableView)
+        setArticleTableView()
+
+        articleTableView.register(ArticleCell.self, forCellReuseIdentifier: "Cell")
+//        self.viewModel.getArticleList(page: viewModel.page, listName: listName)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    private func pagination() {
+        print("page: \(viewModel.page)")
+        viewModel.page += 1
+        viewModel.getArticleList(page: viewModel.page, append: true)
+    }
+    
+    private func setArticleTableView() {
+        
+        articleTableView.translatesAutoresizingMaskIntoConstraints = false
+        articleTableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        articleTableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        articleTableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        articleTableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        articleTableView.heightAnchor.constraint(equalTo:self.view.safeAreaLayoutGuide.heightAnchor).isActive = true
+        articleTableView.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor).isActive = true
+        
+        bindTableArticleData()
+        
+    }
+    
+    private func bindTableArticleData() {
+        
+        articleTableView.rx.didScroll.bind{
+            let offset = self.articleTableView.contentOffset.y + self.articleTableView.frame.height
+            let height = self.articleTableView.contentSize.height
+            if offset > height-10 && !self.viewModel.isLoadingMoreData {
+                self.pagination()
+            }
+        }.disposed(by: disposeBag)
+        
+        viewModel.articleList.bind(to: articleTableView.rx.items(cellIdentifier: reuseIdentifier, cellType: ArticleCell.self)) { row, model, cell in
+            print(model)
+            cell.titleLabel.text = model.title
+            let comment = model.commentNum ?? 0
+            let like = model.likeNum ?? 0
+            cell.commentLikeLabel.text = "💬 " + String(comment) + "🧡 " + String(like)
+            let price = model.price!
+            cell.priceLabel.text = "₩ " + String(price)
+            if model.isSold {
+                cell.priceLabel.text = "판매완료"
+            }
+            cell.imageUrl = model.thumbnailImage
+            cell.loadImage()
+            
+            
+        }.disposed(by: disposeBag)
+        articleTableView.rx.setDelegate(self).disposed(by: disposeBag)
+        
+        articleTableView.rx.itemSelected.bind { indexPath in
+                print("asdf")
+            
+            self.articleTableView.deselectRow(at: indexPath, animated: true)
+                guard let article = self.viewModel.getArticleAt(indexPath) else { return }
+                self.sendArticle(article: article)
+            
+        }.disposed(by: disposeBag)                                               
+    }
+    
+    private func sendArticle(article: Article) {
+        print("sendArticle")
+        let controller = ArticleViewController()
+        controller.articleId = article.id
+        controller.articleSelected = article
+        let navVC = ArticleViewController()
+        self.present(navVC, animated: true)
+    }
+    
+    func getArticleImage(urlString: String) -> UIImage {
+        let url = URL(string: urlString)
+        guard let data = try? Data(contentsOf: url!) else {return UIImage(named: "noImageAvailable") ?? UIImage(named: "noImageAvailable")!}
+        return UIImage(data: data) ?? UIImage(named: "noImageAvailable")!
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- WaffleMarket/WaffleMarket/Controller/MypageViewController.swift
+++ WaffleMarket/WaffleMarket/Controller/MypageViewController.swift
@@ -8,15 +8,116 @@
 import Foundation
 import UIKit
 import RxSwift
+
+
 class MypageViewController: UIViewController {
+    
+    let topView = UIView()
     let tableView = UITableView()
+    
+    let profileImageView = UIImageView()
+    let usernameLabel = UILabel()
+    let showProfileBtn = UIButton()
+    let sellListBtn = UIButton()
+    let buyListBtn = UIButton()
+    let likeListBtn = UIButton()
+    let largeConfig = UIImage.SymbolConfiguration(pointSize: 32, weight: .regular, scale: .large)
+    
     let viewModel = MypageViewModel()
     let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        
+        self.view.addSubview(topView)
+        setTopView()
         self.view.addSubview(tableView)
         setTableView()
+    }
+    
+    private func setTopView() {
+        
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        topView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        topView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        topView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        topView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 150).isActive = true
+        
+        topView.addSubview(profileImageView)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: 20).isActive = true
+        profileImageView.topAnchor.constraint(equalTo: topView.topAnchor).isActive = true
+        profileImageView.bottomAnchor.constraint(equalTo: topView.topAnchor, constant: 60).isActive = true
+        profileImageView.trailingAnchor.constraint(equalTo: topView.leadingAnchor, constant: 80).isActive = true
+        profileImageView.image = UIImage(named: "defaultProfileImage")
+        profileImageView.isUserInteractionEnabled = false
+        
+        topView.addSubview(usernameLabel)
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        usernameLabel.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 20).isActive = true
+        usernameLabel.trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -20).isActive = true
+        usernameLabel.topAnchor.constraint(equalTo: topView.topAnchor, constant: 15).isActive = true
+        usernameLabel.bottomAnchor.constraint(equalTo: profileImageView.bottomAnchor).isActive = true
+        usernameLabel.text = "WaffleMarket"
+        usernameLabel.textColor = .black
+        
+        topView.addSubview(showProfileBtn)
+        showProfileBtn.translatesAutoresizingMaskIntoConstraints = false
+        showProfileBtn.leadingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -50).isActive = true
+        showProfileBtn.trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -20).isActive = true
+        showProfileBtn.topAnchor.constraint(equalTo: usernameLabel.topAnchor).isActive = true
+        showProfileBtn.bottomAnchor.constraint(equalTo: usernameLabel.bottomAnchor).isActive = true
+        showProfileBtn.setImage(UIImage(systemName: "arrow.forward"), for: .normal)
+
+//        showProfileBtn.rx.tap.bind{
+//            let vc = ProfileViewController()
+//            // TODO: article에서 id 받아와서 프로필 찾고 보내기
+//            self.present(vc, animated: true)
+//        }.disposed(by: disposeBag)
+        
+        topView.addSubview(sellListBtn)
+        sellListBtn.translatesAutoresizingMaskIntoConstraints = false
+        sellListBtn.leadingAnchor.constraint(equalTo: topView.centerXAnchor, constant: -110).isActive = true
+        sellListBtn.trailingAnchor.constraint(equalTo: topView.centerXAnchor, constant: -60).isActive = true
+        sellListBtn.topAnchor.constraint(equalTo: showProfileBtn.bottomAnchor, constant: 20).isActive = true
+        sellListBtn.setImage(UIImage(systemName: "line.horizontal.3.circle.fill", withConfiguration: largeConfig), for: .normal)
+        
+        sellListBtn.rx.tap.bind {
+            self.sendList(listName: "sell")
+        }.disposed(by: disposeBag)
+        
+        topView.addSubview(buyListBtn)
+        buyListBtn.translatesAutoresizingMaskIntoConstraints = false
+        buyListBtn.leadingAnchor.constraint(equalTo: sellListBtn.trailingAnchor, constant: 20).isActive = true
+        buyListBtn.trailingAnchor.constraint(equalTo: sellListBtn.trailingAnchor, constant: 80).isActive = true
+        buyListBtn.topAnchor.constraint(equalTo: sellListBtn.topAnchor).isActive = true
+        buyListBtn.setImage(UIImage(systemName: "bag.circle.fill", withConfiguration: largeConfig), for: .normal)
+        
+        buyListBtn.rx.tap.bind {
+            self.sendList(listName: "buy")
+        }.disposed(by: disposeBag)
+        
+        topView.addSubview(likeListBtn)
+        likeListBtn.translatesAutoresizingMaskIntoConstraints = false
+        likeListBtn.leadingAnchor.constraint(equalTo: buyListBtn.trailingAnchor, constant: 20).isActive = true
+        likeListBtn.trailingAnchor.constraint(equalTo: buyListBtn.trailingAnchor, constant: 80).isActive = true
+        likeListBtn.topAnchor.constraint(equalTo: buyListBtn.topAnchor).isActive = true
+        likeListBtn.setImage(UIImage(systemName: "heart.circle.fill", withConfiguration: largeConfig), for: .normal)
+        
+        likeListBtn.rx.tap.bind {
+            self.sendList(listName: "like")
+        }.disposed(by: disposeBag)
+        
+        
+    }
+    
+    private func sendList(listName: String) {
+        print("sendList")
+        let controller = ArticleListViewController()
+        controller.listName = listName
+        let navVC = ArticleListViewController()
+        self.present(navVC, animated: true)
     }
     
     private func setTableView(){
@@ -25,7 +126,7 @@ class MypageViewController: UIViewController {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: topView.bottomAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         
         tableView.register(MypageProfileCell.self, forCellReuseIdentifier: MypageProfileCell.identifier)
